a1d73a698e51100e1778adf0b553a1284afc0d1e
richard <richard.h.passos@hotmail.com>
2019-09-08 19:52:58 -0300

    feat(user): created createUser route

--- __tests__/unit/user.test.js
+++ __tests__/unit/user.test.js
@@ -1,10 +1,29 @@
 const bcrypt = require('bcryptjs');
 const {User} = require('../../src/app/models');
 const truncate = require('../utils/truncate');
+const request = require('supertest');
+const factory = require('../factories');
+const app = require('../../src/app');
 
 describe('User', () => {
     beforeEach(async () => {
         await truncate();
+    });
+    it('should create user', async () => {
+        const {name, email, password} = await factory.create('User');
+        const response = await request(app)
+            .post('/createUser')
+            .send({ name, email, password });
+
+        expect(response.status).toBe(200);
+    });
+
+    it('should not create user with missing params', async () => {
+        const response = await request(app)
+            .post('/createUser')
+            .send({ name: 'missing' });
+
+        expect(response.status).toBe(401);
     });
 
     it('should not create user without password', async () => {
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,20 @@
 const routes = require('express').Router();
 const SessionController = require('./app/controllers/SessionController');
 const authMiddleware = require('../src/app/middleware/auth');
+const { User } = require('./app/models');
 
+routes.post('/createUser', async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        await User.create({ name,email,password });
+        
+        return res.status(200).json({message: 'User created successfully'});
+    } catch (err) {
+        console.log('ERROR:', JSON.stringify(err));
+        return res.status(401).json({message: 'User not created'});
+    }
+});
 
 routes.post('/sessions',SessionController.store);
 
